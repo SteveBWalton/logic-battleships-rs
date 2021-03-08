ab974971944c90027d2f09672e8196a3a0a0d156
Steve Walton <37951256+SteveBWalton@users.noreply.github.com>
2021-03-08 07:32:54 +0000

    Added game 72.

--- src/load_game.rs
+++ src/load_game.rs
@@ -2305,6 +2305,33 @@ impl Game {
                 self.vertical[9] = 0;
             }
 
+           72 => {
+                self.grid = 10;
+                self.maxShip = 4;
+                self.initialise();
+
+                self.horizontal[0] = 1;
+                self.horizontal[1] = 4;
+                self.horizontal[2] = 2;
+                self.horizontal[3] = 2;
+                self.horizontal[4] = 0;
+                self.horizontal[5] = 2;
+                self.horizontal[6] = 5;
+                self.horizontal[7] = 2;
+                self.horizontal[8] = 0;
+                self.horizontal[9] = 2;
+                self.vertical[0] = 0;
+                self.vertical[1] = 4;
+                self.vertical[2] = 1;
+                self.vertical[3] = 3;
+                self.vertical[4] = 4;
+                self.vertical[5] = 0;
+                self.vertical[6] = 0;
+                self.vertical[7] = 7;
+                self.vertical[8] = 0;
+                self.vertical[9] = 1;
+            }
+
         _ => {
                 // This is game 2.
                 self.grid = 10;
